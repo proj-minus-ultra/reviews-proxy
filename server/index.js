c40e5df14ce60c4164fb6d73c197c3cdbfb89c6f
Chris Sanchez <chrissanchez89@gmail.com>
2020-05-15 11:18:39 -0700

    Fixes code to pull fonts from s3

--- server/index.js
+++ server/index.js
@@ -1,26 +1,8 @@
-// const http = require('http');
-// const httpProxy = require('http-proxy');
-
-// var proxy = httpProxy.createProxyServer({});
-
-// //
-// // Create your custom server and just call `proxy.web()` to proxy
-// // a web request to the target passed in the options
-// // also you can use `proxy.ws()` to proxy a websockets request
-// //
-// var server = http.createServer(function(req, res) {
-//   // You can define here your custom logic to handle the request
-//   // and then proxy the request.
-//   proxy.web(req, res, { target: 'http://localhost:3000' });
-// });
-
-// console.log("listening on port 3000")
-// server.listen(3000);
-
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const path = require('path');
+
 const port = 3000
 
 const app = express()
