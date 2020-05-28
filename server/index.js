48e780f3d92f08912907a20545eff56daba90c63
Alex Benko <benkoalex521@gmail.com>
2020-05-28 13:49:35 -0700

    Used new proxy middleware and successfully retrieves data from my component

--- server/index.js
+++ server/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const path = require('path');
-const proxy = require('express-http-proxy');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
 
 const port = 3000
 
@@ -16,13 +17,9 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(express.static(path.join(__dirname, '../public')))
 
-
+//Proxying - routes requests to proper service url
 //REVIEWS (make sure to update url for EC2)//
-
-app.use('/reviews/:rev_Id', proxy('http://localhost:9000/reviews/:rev_Id'));
-
-
-
+app.use('/reviews/:rev_Id', createProxyMiddleware({ target: 'http://localhost:9000', changeOrigin: true }));
 
 app.listen(port, () => console.log(`Proxy listening at http://localhost:${port}`))
 
