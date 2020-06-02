c0047499b38ef66fd91a362a6dcc51acaa6a1466
Alex Benko <benkoalex521@gmail.com>
2020-06-01 18:31:08 -0700

    Docker files set up

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const path = require('path');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 
-const port = 3000
+const port = 4000
 
 const app = express()
 
@@ -15,11 +15,12 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 
-app.use(express.static(path.join(__dirname, '../public')))
+app.use(express.static(path.join(__dirname, '../public'),{maxAge:17536000}))
 
-//Proxying - routes requests to proper service url
+//Proxying - routes requests to proper service url - will possibly have to do this for every endpoint
 //REVIEWS (make sure to update url for EC2)//
-app.use('/reviews/:rev_Id', createProxyMiddleware({ target: 'http://localhost:9000', changeOrigin: true }));
+app.use('/reviews/:rev_Id', createProxyMiddleware({ target: 'http://ec2-35-160-221-45.us-west-2.compute.amazonaws.com/', changeOrigin: true}));
+//http://localhost:3000/reviews/412312 => http://ec2-35-160-221-45.us-west-2.compute.amazonaws.com/reviews/412312
 
 app.listen(port, () => console.log(`Proxy listening at http://localhost:${port}`))
 
